98a1692616b4c0a2a828f75adfd7f264b21f7914
casderooij <hello@casderooij.nl>
2021-11-21 21:50:42 +0100

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import adapterStatic from '@sveltejs/adapter-static';
+import sveltePreprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: sveltePreprocess(),
+
+	kit: {
+		adapter: adapterStatic(),
+		target: '#svelte',
+		vite: {
+			resolve: {
+				alias: {
+					$data: path.resolve('./src/data')
+				}
+			},
+			plugins: []
+		}
+	}
+};
+
+export default config;
